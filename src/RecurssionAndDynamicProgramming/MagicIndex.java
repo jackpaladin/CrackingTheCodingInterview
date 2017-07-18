d32debc6834262865790b6ec59ddd28e855763be
Jack Paladin <jack.a.paladin@gmail.com>
2017-07-18 00:00:41 -0500

    Add magic index

---
+++ src/RecurssionAndDynamicProgramming/MagicIndex.java
@@ -0,0 +1,63 @@
+package RecurssionAndDynamicProgramming;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+/**
+ * Created by jackpaladin on 7/16/17.
+ */
+public class MagicIndex {
+
+    /*
+    Takes advantage of the fact that the array is sorted and there
+    are no duplicates. As this is the case we can use binary search,
+    if the value is greater than the index, a magic index must be to
+    its left and visa versa.
+    Time Complexity: O(logn)
+     */
+    public static int findIndex(int[] arr) {
+        if(arr == null || arr.length == 0) return -1;
+        return findIndexHelper(arr, 0);
+    }
+
+    public static int findIndexHelper(int[] arr, int startIndex) {
+        if(arr.length == 1) {
+            if(arr[0] == startIndex) return startIndex;
+            return -1;
+        }
+        int midIndex = arr.length/2;
+        if(arr[midIndex] == midIndex+startIndex) {
+            return midIndex + startIndex;
+        } else if(arr[midIndex] < midIndex) {
+            return findIndexHelper(Arrays.copyOfRange(arr, midIndex + 1, arr.length), midIndex+1);
+        } else {
+            return findIndexHelper(Arrays.copyOfRange(arr, 0, midIndex), startIndex);
+        }
+    }
+
+    @Test
+    public void nullTest() {
+        assertEquals(-1, findIndex(null));
+    }
+
+    @Test
+    public void empytTest() {
+        int[] emptyArr = {};
+        assertEquals(-1, findIndex(emptyArr));
+    }
+
+    @Test
+    public void trueTest() {
+        int[] trueArr1 = {-2, -1, 0, 3, 8};
+        int[] trueArr2 = {-1, 1, 4, 5, 7, 12, 45};
+        assertEquals(3, findIndex(trueArr1));
+        assertEquals(1, findIndex(trueArr2));
+    }
+
+    @Test
+    public void falseTest() {
+        int[] falseArr = {2,3,4,5,6,7};
+        assertEquals(-1, findIndex(falseArr));
+    }
+}
